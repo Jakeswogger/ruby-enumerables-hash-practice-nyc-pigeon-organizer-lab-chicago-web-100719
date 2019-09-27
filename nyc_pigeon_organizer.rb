1b52e70e357de03fd7ee2d0e66d7b3abc8adf5e8
Jake Swogger <jakeswogger1@gmail.com>
2019-09-27 00:56:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {} 
-  data.each do |a, b| 
-   b.each do |ya, ok|
+  data.each do |birdprimary, aboutbirds| 
+   aboutbirds.each do |ya, ok|
     ok.each do |names|
     
     if !pigeon_list.key? names
